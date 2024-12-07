8ebc8a8c0fdd217ac02f8457efac1763576ae546
Vladislav Ashtaev <AshtaevVV@loodsen.ru>
2024-12-07 23:37:03 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ashtaev/golib2
+
+go 1.22.0
+
+require github.com/ashtaev/golib v0.0.0-20241207175722-21d6cd3b7b67 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package golib2package
+
+import (
+	"github.com/ashtaev/golib"
+)
+
+func Func1() string {
+	return "Module: github.com/ashtaev/golib2, package: golib2package, func: Func1(), version: 1, dependenсies: [" + golib.Func1() + "]"
+}
